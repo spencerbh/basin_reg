35c0fb4afd144cc38081b8f46d4059b0a24ba2d0
spencerbh <spencerbh@basinlogix.com>
2022-04-04 13:29:24 -0700

    rsvp-form

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,4 +398,38 @@
 						$main._show(location.hash.substr(1), true);
 					});
 
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone')
+
+name.addEventListener('blur', valName);
+email.addEventListener('blur', valEmail);
+phone.addEventListener('blur', valPhone);
+
+function valName() {
+	const re = /^[a-zA-Z]{2,20}$/;
+	if(!re.test(name.value)) {
+	name.classList.add('is-invalid');
+	} else {
+	name.classList.remove('is-invalid');
+	}
+}
+
+function valEmail() {
+	const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+	if(!re.test(email.value)) {
+	email.classList.add('is-invalid');
+	} else {
+	email.classList.remove('is-invalid');
+	}
+}
+
+function valPhone() {
+	const re = /^[0-9\-\+]{9,15}$/;
+	if(!re.test(phone.value)) {
+	phone.classList.add('is-invalid');
+	} else {
+	phone.classList.remove('is-invalid');
+	}
+}
 })(jQuery);
